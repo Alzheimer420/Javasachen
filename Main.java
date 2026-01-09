b1eb8f2d3f7e137359ab5f0d329ea4c1efc75221
Simon <it23_skar@bappassau.onmicrosoft.com>
2026-01-09 11:55:43 +0100

    Update Main.java
    bot eingebunden

--- Main.java
+++ Main.java
@@ -4,36 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         Scanner scanner = new Scanner(System.in);
+        ComputerPlayer bot = new ComputerPlayer('O'); // Unser Computer-Gegner
+        
         char currentPlayer = 'X';
         int turns = 0;
 
         while (turns < 9) {
             board.display();
-            System.out.print("Spieler " + currentPlayer + ", Feld (1-9): ");
-            
-            if (scanner.hasNextInt()) {
-                int input = scanner.nextInt() - 1;
+            int move;
 
-                if (board.setField(input, currentPlayer)) {
-                    
-                    if (board.checkWin(currentPlayer)) {
-                        board.display();
-                        System.out.println("Glückwunsch! Spieler " + currentPlayer + " hat gewonnen!");
-                        return; 
-                    }
-                    
-                    turns++;
-                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-                } else {
-                    System.out.println("Ungültiger Zug!");
-                }
+            if (currentPlayer == 'X') {
+                
+                System.out.print("Dein Zug (1-9): ");
+                move = scanner.nextInt() - 1;
             } else {
-                System.out.println("Bitte eine Zahl eingeben!");
-                scanner.next();
+                
+                System.out.println("Computer (O) überlegt...");
+                move = bot.getMove();
             }
-        }
 
-        board.display();
-        System.out.println("Unentschieden! Das Feld ist voll.");
+            
+            if (board.setField(move, currentPlayer)) {
+                if (board.checkWin(currentPlayer)) {
+                    board.display();
+                    System.out.println(currentPlayer + " hat gewonnen!");
+                    return;
+                }
+                
+                
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                turns++;
+            } else if (currentPlayer == 'X') {
+                
+                System.out.println("Feld belegt! Versuch es nochmal.");
+            }
+            
+        }
+        System.out.println("Unentschieden!");
     }
 }
