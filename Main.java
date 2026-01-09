eae53464f3d34bd53aae7162ba6c101c4be085d2
Simon <it23_skar@bappassau.onmicrosoft.com>
2026-01-09 11:04:44 +0100

    First Main.java Commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Tic Tac Toe - Initialisierung...");
+        Board board = new Board();
+        board.display();
+    }
+}
