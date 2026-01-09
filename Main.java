c369d4d034c7fa93eea19763b340f584efd3e74f
Simon <it23_skar@bappassau.onmicrosoft.com>
2026-01-09 11:17:16 +0100

    Update Main.java
    Eingabe Spieler X

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tic Tac Toe - Initialisierung...");
         Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+        
+        board.display();
+        
+        System.out.print("Spieler X, wähle ein Feld (1-9): ");
+        int input = scanner.nextInt();
+        
+       
+        if (board.setField(input - 1, 'X')) {
+            System.out.println("Zug erfolgreich!");
+        } else {
+            System.out.println("Ungültiger Zug!");
+        }
+        
         board.display();
     }
 }
