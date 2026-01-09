80a647f328b562c0150cce4d4bb0a9612105998c
Simon <it23_skar@bappassau.onmicrosoft.com>
2026-01-09 11:29:21 +0100

    Update Main.java
    while schleife mit zwei spieler (noch ohne gewinnen)

--- Main.java
+++ Main.java
@@ -5,18 +5,35 @@ public class Main {
         Board board = new Board();
         Scanner scanner = new Scanner(System.in);
         
-        board.display();
+        char currentPlayer = 'X';
+        int turns = 0;          
+
+        System.out.println("Das Spiel beginnt!");
+
         
-        System.out.print("Spieler X, wähle ein Feld (1-9): ");
-        int input = scanner.nextInt();
-        
-       
-        if (board.setField(input - 1, 'X')) {
-            System.out.println("Zug erfolgreich!");
-        } else {
-            System.out.println("Ungültiger Zug!");
+        while (turns < 9) {
+            board.display();
+            System.out.print("Spieler " + currentPlayer + ", wähle ein Feld (1-9): ");
+            
+            
+            if (scanner.hasNextInt()) {
+                int input = scanner.nextInt() - 1;
+
+                
+                if (board.setField(input, currentPlayer)) {
+                    turns++;
+                    
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                } else {
+                    System.out.println("Zug ungültig! Das Feld ist belegt oder existiert nicht.");
+                }
+            } else {
+                System.out.println("Bitte gib eine Zahl ein!");
+                scanner.next();
+            }
         }
-        
+
         board.display();
+        System.out.println("Spiel beendet. Unentschieden (oder jemand hat gewonnen, aber wir prüfen es noch nicht)!");
     }
 }
