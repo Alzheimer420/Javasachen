2571f2d0f779b0a31b41f46d5543faacea351726
Simon <it23_skar@bappassau.onmicrosoft.com>
2026-01-09 11:45:55 +0100

    Update Main.java
    nach jedem zug wird jetzt geschaut ob der spieler gewonnen hat

--- Main.java
+++ Main.java
@@ -4,36 +4,36 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         Scanner scanner = new Scanner(System.in);
-        
         char currentPlayer = 'X';
-        int turns = 0;          
+        int turns = 0;
 
-        System.out.println("Das Spiel beginnt!");
-
-        
         while (turns < 9) {
             board.display();
-            System.out.print("Spieler " + currentPlayer + ", wähle ein Feld (1-9): ");
-            
+            System.out.print("Spieler " + currentPlayer + ", Feld (1-9): ");
             
             if (scanner.hasNextInt()) {
                 int input = scanner.nextInt() - 1;
 
-                
                 if (board.setField(input, currentPlayer)) {
-                    turns++;
                     
+                    if (board.checkWin(currentPlayer)) {
+                        board.display();
+                        System.out.println("Glückwunsch! Spieler " + currentPlayer + " hat gewonnen!");
+                        return; 
+                    }
+                    
+                    turns++;
                     currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
                 } else {
-                    System.out.println("Zug ungültig! Das Feld ist belegt oder existiert nicht.");
+                    System.out.println("Ungültiger Zug!");
                 }
             } else {
-                System.out.println("Bitte gib eine Zahl ein!");
+                System.out.println("Bitte eine Zahl eingeben!");
                 scanner.next();
             }
         }
 
         board.display();
-        System.out.println("Spiel beendet. Unentschieden (oder jemand hat gewonnen, aber wir prüfen es noch nicht)!");
+        System.out.println("Unentschieden! Das Feld ist voll.");
     }
 }
